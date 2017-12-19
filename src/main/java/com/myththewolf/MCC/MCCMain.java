50f260f5ee8a1e68ed1a287bd337573681cf5e2d
MythTheWolf <myth.wolf6@gmail.com>
2017-12-19 00:19:22 -0600

    Can't forget that lol

--- src/main/java/com/myththewolf/MCC/MCCMain.java
+++ src/main/java/com/myththewolf/MCC/MCCMain.java
@@ -5,10 +5,7 @@ import com.myththewolf.BotServ.lib.API.invoke.PluginAdapter;
 import com.myththewolf.MCC.commands.SetChatChan;
 import com.myththewolf.MCC.commands.setInfoChannel;
 import com.myththewolf.MCC.lib.TCPServer;
-import com.myththewolf.MCC.lib.messageListeners.JDAChatListener;
-import com.myththewolf.MCC.lib.messageListeners.ServerOfflineListener;
-import com.myththewolf.MCC.lib.messageListeners.ServerOnlineMessageListener;
-import com.myththewolf.MCC.lib.messageListeners.UserJoinListener;
+import com.myththewolf.MCC.lib.messageListeners.*;
 
 public class MCCMain implements PluginAdapter {
     public static BotPlugin plugin;
@@ -36,6 +33,7 @@ public class MCCMain implements PluginAdapter {
         srv.registerPacketListener("server-online", new ServerOnlineMessageListener());
         srv.registerPacketListener("server", new ServerOfflineListener());
         srv.registerPacketListener("user-join", new UserJoinListener());
+        srv.registerPacketListener("user-chat", new UserChatListener());
         try {
             arg0.registerCommand(";setinfochan", new setInfoChannel());
             arg0.registerCommand(";setchatchan", new SetChatChan());
