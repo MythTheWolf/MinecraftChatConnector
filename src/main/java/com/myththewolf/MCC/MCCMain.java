f646e02f5d96d3bbc7f0af68e2963240c415a947
MythTheWolf <myth.wolf6@gmail.com>
2017-12-19 21:09:22 -0600

    fixed oopsie

--- src/main/java/com/myththewolf/MCC/MCCMain.java
+++ src/main/java/com/myththewolf/MCC/MCCMain.java
@@ -2,6 +2,8 @@ package com.myththewolf.MCC;
 
 import com.myththewolf.BotServ.lib.API.invoke.BotPlugin;
 import com.myththewolf.BotServ.lib.API.invoke.PluginAdapter;
+import com.myththewolf.MCC.PacketHandlers.UserChatHandler;
+import com.myththewolf.MCC.lib.SocketServer;
 
 
 public class MCCMain implements PluginAdapter {
@@ -16,7 +18,9 @@ public class MCCMain implements PluginAdapter {
     @Override
     public void onEnable(BotPlugin arg0) {
         plugin = arg0;
-
+        SocketServer SS = new SocketServer(6789);
+        SS.startServer();
+        SS.registerPacketHandler("user-chat", new UserChatHandler());
     }
 
 }
